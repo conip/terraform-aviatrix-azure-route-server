741d5a77ff7c7272345a693c4a1b54b22b134c41
conip <51626146+conip@users.noreply.github.com>
2022-11-16 18:18:33 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "vng" {
   value = azurerm_virtual_network_gateway.default
 }
+
+output "vnet" {
+  value = azurerm_virtual_network.default
+}
+
+output "route_server" {
+  value = azurerm_route_server.default
+}
